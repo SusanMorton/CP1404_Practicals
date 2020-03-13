370ed2d643caa4ace29911eadc0a5f886ed92154
SusanMorton <susan.morton1@my.jcu.edu.au>
2020-03-13 11:49:00 +1000

    add random import to generate score

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -4,13 +4,17 @@ Broken program to determine score status
 """
 import random
 def main():
+
     score = float(input("Enter score: "))
     while score < 0 or score > 100:
         print("Invalid score")
         score = float(input("Enter score: "))
     score_result = result_checker(score)
     print(score_result)
-
+    random_score = random.randint(0, 100)
+    print(random_score)
+    random_result = result_checker(random_score)
+    print(random_result)
 
 
 def result_checker(score):
