bd9e46f082396c48b5a65223bd266d5f30dd5022
SusanMorton <susan.morton1@my.jcu.edu.au>
2020-03-17 19:21:42 +1000

    completed initial program with number checker

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -7,6 +7,8 @@ def main():
     print("The first number is {}".format(numbers[0]))
     print("The last number is {}".format(numbers[-1]))
     print("The smallest number is {}".format(min(numbers)))
+    print("The largest number is {}".format(max(numbers)))
+    print("The average of the numbers is {}".format((sum(numbers)/len(numbers))))
 
 
 
