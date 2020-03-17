a095f3bbcf7b491641cf4ada9fc641c04743d422
SusanMorton <susan.morton1@my.jcu.edu.au>
2020-03-18 08:47:10 +1000

    created lists to contain each pick round

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -3,18 +3,17 @@ import random
 def main():
 
     number_list = []
-    numbers_of_lines = int(input("How many quick pick lines do you want?:" ))
-    valid_numbers = 0
+    numbers_of_lines = int(input("How many quick picks? " ))
+
     for number_of_lines in range(numbers_of_lines):
-        number_generator = random.randint(1,45)
-        if number_generator in number_list:
-            print("invalid number {}".format(number_generator))
-            #number_generator = random.randint(1, 45)
-            #print("new number {}".format(number_generator))
-        else:
-            print(number_generator)
-            number_list.append(number_generator)
-            print(number_list)
-            valid_numbers +=1
+        individual_number_list = []
+        for i in range(6):
+            number = random.randint(1,45)
+            individual_number_list.append(number)
+        print(individual_number_list)
+        number_list.append(individual_number_list)
+    print(number_list)
+
+
 
 main()
