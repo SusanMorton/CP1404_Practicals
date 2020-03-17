4ba754f79307047d3955d87c0b2430df53a25dba
SusanMorton <susan.morton1@my.jcu.edu.au>
2020-03-18 08:56:37 +1000

    adding error checking for numbers in lists of 6

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -7,9 +7,17 @@ def main():
 
     for number_of_lines in range(numbers_of_lines):
         individual_number_list = []
+        #individual_number_list = [number for number in range(6) not in individual_number_list]
+        valid_number = 0
         for i in range(6):
-            number = random.randint(1,45)
-            individual_number_list.append(number)
+            while valid_number !=6:
+                number = random.randint(1,45)
+                print("current list is {}".format(individual_number_list))
+                if number not in individual_number_list:
+                    individual_number_list.append(number)
+                    valid_number +=1
+                else:
+                    print("invalid number {}".format(number))
         print(individual_number_list)
         number_list.append(individual_number_list)
     print(number_list)
