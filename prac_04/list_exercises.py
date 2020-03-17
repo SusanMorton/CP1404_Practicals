972a0627137554190b77882e97ec3a7f8ebb1aa2
SusanMorton <susan.morton1@my.jcu.edu.au>
2020-03-17 19:18:09 +1000

    adding the first and last numbers. commiting before troubleshooting minimum

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,14 @@
+def main():
+    numbers = []
+    for i in range(5):
+        number = int(input("Number: "))
+        numbers.append(number)
+
+    print("The first number is {}".format(numbers[0]))
+    print("The last number is {}".format(numbers[-1]))
+    print("The smallest number is {}".format(min.numbers))
+
+
+
+
+main()
