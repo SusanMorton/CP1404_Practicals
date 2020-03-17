838f9b1594deacbae7df7e47379a28db0807df05
SusanMorton <susan.morton1@my.jcu.edu.au>
2020-03-17 19:20:02 +1000

    adding minimum number checker

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -6,7 +6,7 @@ def main():
 
     print("The first number is {}".format(numbers[0]))
     print("The last number is {}".format(numbers[-1]))
-    print("The smallest number is {}".format(min.numbers))
+    print("The smallest number is {}".format(min(numbers)))
 
 
 
