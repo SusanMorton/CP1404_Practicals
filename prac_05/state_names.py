67f36d4e64b6c943ee08066416e997a4a7d13f5e
SusanMorton <susan.morton1@my.jcu.edu.au>
2020-03-27 15:06:05 +1000

    Updating formatting issues to get green tick

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,9 +4,8 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {'QLD': 'Queensland', 'NSW': 'New South Wales', 'NT': "Northern Territory", 'WA': "Western Australia",
-                'ACT': "Australian Capital Territory", 'VIC': "Victoria", 'TAS': "Tasmania"}
+CODE_TO_NAME = {'QLD': 'Queensland', 'NSW': 'New South Wales', 'NT': 'Northern Territory', 'WA': 'Western Australia',
+                'ACT': "Australian Capital Territory", 'VIC': 'Victoria', 'TAS': 'Tasmania'}
 print(CODE_TO_NAME)
 
 state_code = input("Enter short state: ").upper()
@@ -17,6 +16,5 @@ while state_code != "":
         print("Invalid short state")
     state_code = input("Enter short state: ").upper()
 
-for short,long in CODE_TO_NAME.items():
-    print("{:<3} is {}".format(short,long))
-
+for short, long in CODE_TO_NAME.items():
+    print("{:<3} is {}".format(short, long))
