27122077a19f041bbfecbc332582fed252e9d27a
SusanMorton <susan.morton1@my.jcu.edu.au>
2020-03-28 09:52:06 +1000

    sorted dictionary into new list, found maximum length word

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,11 +1,26 @@
 
-word_counter = {}
-usertext = input('Enter a sentence here: ')
-
-for word in usertext:
-    if word in word_counter:
-        word_counter[word] += 1
+word_dict = {}
+#userwords = input('Text: ')
+userwords = "this is a colletion of words of nice words this is a fun thing it is"
+for word in userwords.split():
+    if word in word_dict:
+        word_dict[word] += 1
     else:
-        word_counter[word] = 1
+        word_dict[word] = 1
 
-print(word_counter)
+print(word_dict)
+
+for words,count in word_dict.items():
+    print("{} : {}".format(words, count))
+
+word_list = []
+
+for singleword in word_dict:
+    word_list.append(singleword)
+
+print(word_list)
+word_list.sort()
+print(word_list)
+
+max_word_length = max(len(word) for word in word_list)
+print(max_word_length)
